69d04e15873ce0d20c968cf86fd3431ad8c136da
Arsene Ngenzi <arsenengenzi@gmail.com>
2022-06-14 13:08:04 +0200

    added classes to change backgrounds

--- src/App.js
+++ src/App.js
@@ -14,8 +14,12 @@ function App() {
     if(evt.key === 'Enter') {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
-        .then(data => setWeather(data))
-    }
+        .then(data => {
+          setWeather(data)
+          setQuery('')
+          console.log(data)
+        })
+      }
   }
 
 
@@ -32,27 +36,37 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
-        <div className="search-box">
-          <input type="text" 
-          className="search-bar"
-          placeholder="Search..."
+        <div className='search-box'>
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Type Any City...'
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
 
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
+        {typeof weather.main != 'undefined' ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
 
-        <div className="weather-box">
-          <div className="temp">15°C</div>
-          <div className="weather">Sunny</div> 
-        </div>
+            <div className='weather-box'>
+              <div className='temp'>{Math.round(weather.main.temp)}°C</div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </main>
     </div>
-  );
+  )
 }
 
 export default App;
